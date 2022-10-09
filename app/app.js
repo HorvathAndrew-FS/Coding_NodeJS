82aeba2e25b399c032263f686fb806752043fde7
HorvathAndrew-FS <skwerl@student.fullsail.edu>
2022-10-09 13:47:26 -0500

    updating files

--- app/app.js
+++ app/app.js
@@ -12,9 +12,24 @@ app.get('/', (req,res,next) => {
     });
 });
 //req.body.firstName
-
 //cors
+//localhost:3000/example
+app.use('/example', router);
 
-app.use('/example', router)
+//add middleware to handle errors and bad urls
+app.use((req, res, next) => {
+    const error = new Error("NOT FOUND!");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error:{
+            message: error.message,
+            status: error.status,
+        },
+    });
+});
 
 module.exports = app;
--- router/router.js
+++ router/router.js
@@ -1,4 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+router.get("/", (req, res)=> {
+    res.status(200).json({
+        message: "GET - SUCCESS",
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
+
+//localhost:3000/example/
+router.get("/:id", (reg, res)=> {
+    const id = req.params.id;
+    res.status(200).json({
+        message: "GET - SUCCESS",
+        id: id,
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+    });
+});
+
 module.exports = router;
