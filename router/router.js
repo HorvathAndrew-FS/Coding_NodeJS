deaeab1b463a461bae0d107ade3e703f2d9d4bee
HorvathAndrew-FS <skwerl@student.fullsail.edu>
2022-10-09 13:55:27 -0500

    update router file

--- router/router.js
+++ router/router.js
@@ -24,4 +24,17 @@ router.get("/:id", (reg, res)=> {
     });
 });
 
+//localhost:3000/example/23
+router.get('/:id', (req,res)=> {
+    const id = req.params.id;
+    res.status(200).json({
+        message: "GET - SUCCESS",
+        id: id,
+        metadata: {
+            hostname: req.hostname,
+            method: req.method,
+        },
+    })
+})
+
 module.exports = router;
